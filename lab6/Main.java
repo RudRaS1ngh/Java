475968503757c9cf3b57361ce1ef2397b460d191
RudRaS1ngh <rudrasingh.cs24@bmsce.ac.in>
2025-11-18 11:03:56 +0530

    Add files via upload

---
+++ lab6/Main.java
@@ -0,0 +1,12 @@
+import SEE.External;
+
+class Main{
+    public static void main(String args[]){
+        External e=new External();
+        e.inputDetails();
+        e.CIEmarks();
+        e.SEEmarks();
+        e.Details();
+        e.Final_marks(e);
+    }
+}
